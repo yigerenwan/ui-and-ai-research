42a8aea624a49fedbef3e3bc04b2977cd999979f
yigerenwan <raugenai@gmail.com>
2025-04-11 12:44:34 +0000

    update

---
+++ ai-models/image-text-to-image/uno/readme.md
@@ -0,0 +1,13 @@
+# UNO - Modified based on the original [UNO](https://github.com/bytedance/UNO)
+
+Install the dependencies:
+
+```bash
+pip install -r requirements.txt
+```
+
+## Usage
+
+```bash
+python app.py
+```
--- ai-models/text-to-image/highdream-i1/run.sh
+++ ai-models/text-to-image/highdream-i1/run.sh
@@ -1,3 +1,6 @@
+virtualenv -p python3 venv
+source venv/bin/activate
+pip install -r requirements.txt
 set USE_FLASH_ATTENTION=1
 pip uninstall torch
 pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu124
@@ -6,3 +9,5 @@ pip install cmake
 pip install ninja
 pip install wheel
 pip install flash-attn --no-build-isolation 
+pip install gradio
+python gradio_demo.py
